33cf01fcc3b8bc2c484e575d4022127cbeeef00e
Sy <aldwin.c.t.sy@accenture.com>
2018-10-31 16:52:22 +0800

    updated footer

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'facebook',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../../../assets/facebook.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'twitter',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../../../assets/twitter.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'tumblr',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../../../assets/tumblr.svg')
+    );
   }
 
 }
