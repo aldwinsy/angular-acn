972170de0b4ce17942536ede9174b86b63a55788
Sy <aldwin.c.t.sy@accenture.com>
2018-11-07 14:28:17 +0800

    hide banner upon user action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: [
     './app.component.scss',
     './shared/styles/box-styles.scss'
+  ],
+  animations: [
+    trigger('showHideBanner', [
+      state('hide', style({ display: 'none' })),
+      transition('show => hide', [
+        animate('2s', style({ opacity: 0 })),
+      ])
+    ])
   ]
 })
+
 export class AppComponent {
-  title = 'sasi-ui';
+  readonly title = 'sasi-ui';
   darkTheme = false;
+  showBanner = true;
 
   changeTheme(value) {
     this.darkTheme = value;
