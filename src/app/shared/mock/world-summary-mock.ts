2213c0618345cc1b19733aefc1d45088597b474c
Sy <aldwin.c.t.sy@accenture.com>
2018-11-14 19:32:29 +0800

    updated unit test

--- src/app/shared/mock/world-summary-mock.ts
+++ src/app/shared/mock/world-summary-mock.ts
@@ -1,112 +1,112 @@
 export const worldSummaryData = [
   // {
-  //   objectname: 'atcControlledStation',
+  //   objectName: 'atcControlledStation',
   //   purgatoryCount: 0,
   //   paradiseCount: 0
 
   // },
   {
-    objectname: 'aircraft',
+    objectName: 'aircraft',
     purgatoryCount: 100,
     paradiseCount: 100
   },
   {
-    objectname: 'airport',
+    objectName: 'airport',
     purgatoryCount: 200,
     paradiseCount: 200
   },
   {
-    objectname: 'airportGroup',
+    objectName: 'airportGroup',
     purgatoryCount: 4000,
     paradiseCount: 4000
   },
   {
-    objectname: 'airportGroupRelationship',
+    objectName: 'airportGroupRelationship',
     purgatoryCount: 10034,
     paradiseCount: 10034
   },
   {
-    objectname: 'crew',
+    objectName: 'crew',
     purgatoryCount: 500,
     paradiseCount: 500
   },
   {
-    objectname: 'crewBase',
+    objectName: 'crewBase',
     purgatoryCount: 600,
     paradiseCount: 600
   },
   {
-    objectname: 'fleet',
+    objectName: 'fleet',
     purgatoryCount: 700,
     paradiseCount: 700
   },
   {
-    objectname: 'fleetCompatibility',
+    objectName: 'fleetCompatibility',
     purgatoryCount: 50,
     paradiseCount: 50
   },
   {
-    objectname: 'flight',
+    objectName: 'flight',
     purgatoryCount: 10034,
     paradiseCount: 10034
   },
   {
-    objectname: 'flightActivity',
+    objectName: 'flightActivity',
     purgatoryCount: 13400,
     paradiseCount: 13400
   },
   {
-    objectname: 'interSegmentTime',
+    objectName: 'interSegmentTime',
     purgatoryCount: 2100,
     paradiseCount: 2100
   },
   {
-    objectname: 'maintenance',
+    objectName: 'maintenance',
     purgatoryCount: 1100,
     paradiseCount: 1100
   },
   {
-    objectname: 'market',
+    objectName: 'market',
     purgatoryCount: 100,
     paradiseCount: 100
   },
   {
-    objectname: 'nonFly',
+    objectName: 'nonFly',
     purgatoryCount: 10034,
     paradiseCount: 10034
   },
   {
-    objectname: 'offlineTransportations',
+    objectName: 'offlineTransportations',
     purgatoryCount: 10067,
     paradiseCount: 10067
   },
   {
-    objectname: 'pairing',
+    objectName: 'pairing',
     purgatoryCount: 1005,
     paradiseCount: 1005
   },
   {
-    objectname: 'passengerConnection',
+    objectName: 'passengerConnection',
     purgatoryCount: 1006,
     paradiseCount: 1006
   },
   // {
-  //   objectname: 'position',
+  //   objectName: 'position',
   //   purgatoryCount: 3,
   //   paradiseCount: 3
   // },
   {
-    objectname: 'qualification',
+    objectName: 'qualification',
     purgatoryCount: 1004,
     paradiseCount: 1004
   },
   {
-    objectname: 'subfleet',
+    objectName: 'subfleet',
     purgatoryCount: 10012,
     paradiseCount: 10012
   },
   {
-    objectname: 'training',
+    objectName: 'training',
     purgatoryCount: 100,
     paradiseCount: 100
   }
