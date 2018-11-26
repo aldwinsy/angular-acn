ae23f0f58184785c8db9a100520c5b9d541d01e9
kent.m.cabarle <kent.m.cabarle@accenture.com>
2018-11-26 10:03:55 +0800

    1. Added new interface for sasi page - world summary table 2. Transform data

--- src/app/shared/interfaces/world-summary.interface.ts
+++ src/app/shared/interfaces/world-summary.interface.ts
@@ -23,3 +23,24 @@ export interface SasiWorldInterface {
   topLevelObjects: TopLevelObject[];
   total: Total;
 }
+
+// status page
+
+export interface SasiWorldSummary {
+  paradiseEvents: string;
+  paradiseObj: number;
+  propertyName: string;
+  published_0_Events: string;
+  published_0_Obj: number;
+  published_1_Events: string;
+  published_1_Obj: number;
+  published_2_Events: string;
+  published_2_Obj: number;
+  purgatoryEvents: string;
+  purgatoryObj: number;
+  validationExcl: string;
+  validationFixed: string;
+  validationIEPM: string;
+  validationOEPM: string;
+
+}
--- src/app/views/sasi-status/data-status/purgatory-paradise/purgatory-paradise.component.ts
+++ src/app/views/sasi-status/data-status/purgatory-paradise/purgatory-paradise.component.ts
@@ -4,7 +4,7 @@ import { map } from 'rxjs/operators';
 import * as _ from 'lodash';
 import { purgatoryParadiseWorldColumns, purgatoryParadiseTimeColumns, sasiStatusLabels } from 'sasi/shared/variables/global-variables';
 import { SasiStatusService } from 'sasi/views/sasi-status/sasi-status.service';
-import { WorldSummaryInterface, SasiWorldInterface } from 'sasi/shared/interfaces/world-summary.interface';
+import { SasiWorldInterface } from 'sasi/shared/interfaces/world-summary.interface';
 import { CoreService } from 'sasi/core/service/core.service';
 
 
@@ -30,18 +30,54 @@ export class PurgatoryParadiseComponent implements OnInit {
   isDataLoading = false;
 
   // TODO: Insert private coreService: CoreService
-  constructor(private sasiStatusService: SasiStatusService) { }
+  constructor(private coreService: CoreService, private sasiStatusService: SasiStatusService) { }
 
-    ngOnInit() {
-      this.isDataLoading = true;
-      combineLatest(
-        this.sasiStatusService.getSasiStatusTime(),
-        this.sasiStatusService.getSasiStatusWorldObjects()
-      ).subscribe(data => {
-        this.statusTime = data[0];
-        this.worldObjects = data[1];
-        this.isDataLoading = false;
-      });
-    }
+  ngOnInit() {
+    this.isDataLoading = true;
+    this.sasiStatusService.getSasiStatusTime()
+      .subscribe(data => {
+        this.statusTime = data;
+    });
+
+    combineLatest(
+      this.coreService.getPurgatorySummary(),
+      this.coreService.getParadiseSummary()
+    )
+    .pipe(map(([purgatory, paradise]) => this.transformWorldSummary(purgatory, paradise)))
+    .subscribe(data => {
+      this.worldObjects = data;
+      console.log('this.worldObjects', this.worldObjects);
+      this.isDataLoading = false;
+    });
+  }
+
+  transformWorldSummary(purgatory: SasiWorldInterface, paradise: SasiWorldInterface) {
+    // Add logic for total and previous world
+    const purgatoryData = purgatory.topLevelObjects.map(object => {
+      return {
+        propertyName: object.objectName,
+        purgatoryObj: object.objectCount,
+        paradiseEvents: '-',
+        published_0_Events: '-',
+        published_0_Obj: '-',
+        published_1_Events: '-',
+        published_1_Obj: '-',
+        published_2_Events: '-',
+        published_2_Obj: '-',
+        purgatoryEvents: '-',
+        validationExcl: '-',
+        validationFixed: '-',
+        validationIEPM: '-',
+        validationOEPM: '-'
+      };
+    });
+    const paradiseData = paradise.topLevelObjects.map(object => {
+      return {
+        propertyName: object.objectName,
+        paradiseObj: object.objectCount
+      };
+    });
+    return _.merge(purgatoryData, paradiseData);
+  }
 
 }
